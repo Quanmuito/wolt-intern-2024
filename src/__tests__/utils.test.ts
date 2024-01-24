c576c63301dcaa597844d365908376bcf7b3cc0b
quanmuito <tranleminhquan1102@gmail.com>
2024-01-24 13:20:47 +0200

    automate test 1

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,7 +1,78 @@
-import { getCartValueSurcharge } from 'utils';
+import {
+    getCartValueSurcharge,
+    getDeliveryDistanceSurcharge,
+    getNumberOfItemsSurcharge,
+    isRushHour
+} from 'utils';
 
+const cartValueCases = [
+    [8.9, 1.1],
+    [5.0, 5.0],
+    [10.0, 0],
+    [10.1, 0.1],
+    [11.0, 0],
+];
 describe('Test get cart value surcharge', () => {
-    test('Test cart value less than 10', () => {
-        expect(getCartValueSurcharge(8.9).toFixed(2)).toEqual(1.1.toFixed(2));
-    });
+    test.each(cartValueCases)(
+        'Cart value is %p and surcharge amount should be %p',
+        (cartValue, surcharge) => {
+            let result = getCartValueSurcharge(cartValue);
+            expect(result.toFixed(2)).toEqual(surcharge.toFixed(2));
+        }
+    );
+});
+
+const deliveryDistanceCases = [
+    [900, 2.0],
+    [1000, 2.0],
+    [1001, 3.0],
+    [1499, 3.0],
+    [1500, 3.0],
+    [1501, 4.0],
+];
+describe('Test get delivery distance surcharge', () => {
+    test.each(deliveryDistanceCases)(
+        'Delivery distance is %p and surcharge amount should be %p',
+        (deliveryDistance, surcharge) => {
+            let result = getDeliveryDistanceSurcharge(deliveryDistance);
+            expect(result.toFixed(2)).toEqual(surcharge.toFixed(2));
+        }
+    );
+});
+
+const numberOfItemsCases = [
+    [4, 0.0],
+    [5, 0.5],
+    [10, 3.0],
+    [13, 5.7],
+    [14, 6.2],
+];
+describe('Test get number of items surcharge', () => {
+    test.each(numberOfItemsCases)(
+        'Number of items is %p and surcharge amount should be %p',
+        (numberOfItems, surcharge) => {
+            let result = getNumberOfItemsSurcharge(numberOfItems);
+            expect(result.toFixed(2)).toEqual(surcharge.toFixed(2));
+        }
+    );
+});
+
+const rushHourCases: [string, boolean][] = [
+    ['2024-01-25T12:30', false], // not Fri, not rush hour
+    ['2024-01-25T15:30', false], // not Fri, rush hour
+    ['2024-01-26T12:30', false], // Fri, before rush hour
+    ['2024-01-26T15:30', true],
+    ['2024-01-26T16:30', true],
+    ['2024-01-26T17:30', true],
+    ['2024-01-26T18:30', true],
+    ['2024-01-26T19:30', false], // Fri, after rush hour
+];
+describe('Test check rush hour', () => {
+    test.each(rushHourCases)(
+        'Input timestamp is %p and result should be %p',
+        (timestamp, expected) => {
+            let result = isRushHour(timestamp);
+            expect(result).toBe(expected);
+        }
+    );
 });
