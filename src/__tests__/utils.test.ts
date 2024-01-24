c45d79e162283e12e4004c21d95308e74d46f1f2
quanmuito <tranleminhquan1102@gmail.com>
2024-01-24 04:54:54 +0200

    ci1

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -2,6 +2,6 @@ import { getCartValueSurcharge } from 'utils';
 
 describe('Test get cart value surcharge', () => {
     test('Test cart value less than 10', () => {
-        expect(getCartValueSurcharge(8.9)).toEqual(1.0);
+        expect(getCartValueSurcharge(8.9).toFixed(2)).toEqual(1.1.toFixed(2));
     });
 });
