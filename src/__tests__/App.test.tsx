832505148683edb0f6515b6504c918a09831b1a3
quanmuito <tranleminhquan1102@gmail.com>
2024-01-24 16:58:54 +0200

    automation test 5

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -8,7 +8,6 @@ describe('Test render', () => {
         render(<App />);
         expect(await screen.findByText(/delivery fee calculator/i)).toBeInTheDocument();
 
-        expect(await screen.findByLabelText(/delivery distance/i)).toBeInTheDocument();
         expect(await screen.findByLabelText(/number of items/i)).toBeInTheDocument();
         expect(await screen.findByLabelText(/order time/i)).toBeInTheDocument();
         expect(await screen.findByLabelText(/total delivery fee/i)).toBeInTheDocument();
@@ -22,9 +21,30 @@ describe('Test render', () => {
         expect(description).toBeInTheDocument();
 
         userEvent.type(input, '10');
-        expect(input).toHaveValue('10');
+        expect(input).toHaveValue(10);
+
+        userEvent.type(input, '20,6');
+        expect(input).toHaveValue(20.60);
 
         userEvent.type(input, '200');
-        expect(input).toHaveValue('200');
+        expect(input).toHaveValue(200);
+    });
+
+    test('Test delivery distance input', async () => {
+        render(<App />);
+        let input = await screen.findByLabelText(/delivery distance/i);
+        let description = await screen.findByText(/the distance between the store and location of customer in meters/i);
+        expect(input).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+
+        userEvent.type(input, '1000');
+        expect(input).toHaveValue(1000);
+
+        userEvent.type(input, '1500');
+        expect(input).toHaveValue(1500);
+
+        userEvent.type(input, '1000.5');
+        let errorText = await screen.findByText(/invalid input error: please input an integer/i);
+        expect(errorText).toBeInTheDocument();
     });
 });
