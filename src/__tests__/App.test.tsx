8a542a0e43f9d66eab533264daa7cbedb7b93291
quanmuito <tranleminhquan1102@gmail.com>
2024-01-24 17:17:45 +0200

    automation test 6

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,48 +3,63 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-describe('Test render', () => {
-    test('Test heading', async () => {
+test('Test render heading', async () => {
+    render(<App />);
+    expect(await screen.findByText(/delivery fee calculator/i)).toBeInTheDocument();
+
+    expect(await screen.findByLabelText(/number of items/i)).toBeInTheDocument();
+    expect(await screen.findByLabelText(/order time/i)).toBeInTheDocument();
+    expect(await screen.findByLabelText(/total delivery fee/i)).toBeInTheDocument();
+});
+
+describe('Test cart value input', () => {
+    const cartValueCases: [string, number][] = [
+        ['10', 10],
+        ['200', 200],
+        ['20.6', 20.6],
+        ['40,6', 40.6],
+    ];
+    test.each(cartValueCases)(
+        'Cart value is %p and display value should be %p',
+        async (input, expected) => {
+            render(<App />);
+            let cartValueInput = await screen.findByLabelText(/cart value/i);
+            expect(cartValueInput).toBeInTheDocument();
+
+            userEvent.type(cartValueInput, input);
+            expect(cartValueInput).toHaveValue(expected);
+        }
+    );
+});
+
+describe('Test user event on delivery distance input', () => {
+    const deliveryDistanceCases: [string, number][] = [
+        ['500', 500],
+        ['1000', 1000],
+        ['1200', 1200],
+        ['1499', 1499],
+        ['1500', 1500],
+        ['1501', 1501],
+    ];
+    test.each(deliveryDistanceCases)(
+        'Delivery distance is %p and display value should be %p',
+        async (input, expected) => {
+            render(<App />);
+            let deliveryDistanceInput = await screen.findByLabelText(/delivery distance/i);
+            expect(deliveryDistanceInput).toBeInTheDocument();
+
+            userEvent.type(deliveryDistanceInput, input);
+            expect(deliveryDistanceInput).toHaveValue(expected);
+        }
+    );
+
+    test('Test input float instead of integer', async () => {
         render(<App />);
-        expect(await screen.findByText(/delivery fee calculator/i)).toBeInTheDocument();
+        let deliveryDistanceInput = await screen.findByLabelText(/delivery distance/i);
+        expect(deliveryDistanceInput).toBeInTheDocument();
 
-        expect(await screen.findByLabelText(/number of items/i)).toBeInTheDocument();
-        expect(await screen.findByLabelText(/order time/i)).toBeInTheDocument();
-        expect(await screen.findByLabelText(/total delivery fee/i)).toBeInTheDocument();
-    });
-
-    test('Test cart value input', async () => {
-        render(<App />);
-        let input = await screen.findByLabelText(/cart value/i);
-        let description = await screen.findByText(/value of the shopping cart in euros/i);
-        expect(input).toBeInTheDocument();
-        expect(description).toBeInTheDocument();
-
-        userEvent.type(input, '10');
-        expect(input).toHaveValue(10);
-
-        userEvent.type(input, '20,6');
-        expect(input).toHaveValue(20.60);
-
-        userEvent.type(input, '200');
-        expect(input).toHaveValue(200);
-    });
-
-    test('Test delivery distance input', async () => {
-        render(<App />);
-        let input = await screen.findByLabelText(/delivery distance/i);
-        let description = await screen.findByText(/the distance between the store and location of customer in meters/i);
-        expect(input).toBeInTheDocument();
-        expect(description).toBeInTheDocument();
-
-        userEvent.type(input, '1000');
-        expect(input).toHaveValue(1000);
-
-        userEvent.type(input, '1500');
-        expect(input).toHaveValue(1500);
-
-        userEvent.type(input, '1000.5');
-        let errorText = await screen.findByText(/invalid input error: please input an integer/i);
-        expect(errorText).toBeInTheDocument();
+        userEvent.type(deliveryDistanceInput, '1000.5');
+        let errorMessage = await screen.findByText(/invalid input. error: please input an integer/i);
+        expect(errorMessage).toBeInTheDocument();
     });
 });
