c8671abd91c519fc1576d202a9d87279c4a896e9
quanmuito <tranleminhquan1102@gmail.com>
2024-01-27 21:56:10 +0200

    fix test 4

--- src/__tests__/components/Inputs/index.test.tsx
+++ src/__tests__/components/Inputs/index.test.tsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { InputText, InputNumber, InputSubmit } from 'components/Inputs';
+import {
+    InputText,
+    InputNumber,
+    InputDatetime,
+    InputSubmit
+} from 'components/Inputs';
 
 beforeEach(() => {
     cleanup();
 });
+test('Test render InputText', async () => {
+    render(<InputText id="test" options={ { value: 'Test text value', 'aria-label': 'Test input', onChange: () => {} } } />);
+    const input = await screen.findByRole('textbox', { name: 'Test input' });
 
-describe('Test InputText', () => {
-    test('Test render InputText', async () => {
-        render(<InputText id="test" options={ { value: 'Test text value', 'aria-label': 'Test input', onChange: () => {} } } />);
-        const input = await screen.findByRole('textbox', { name: 'Test input' });
-
-        expect(input).toBeInTheDocument();
-        expect(input).toHaveValue('Test text value');
-    });
-
-    test('Test user event of InputText', async () => {
-        render(<InputText id="test" options={ { value: '', 'aria-label': 'Test input', onChange: () => {} } } />);
-        const input = await screen.findByRole('textbox', { name: 'Test input' });
-
-        userEvent.clear(input);
-        userEvent.type(input, 'User type');
-        expect(input).toHaveValue('User type');
-    });
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('Test text value');
 });
 
-describe('Test InputNumber', () => {
-    test('Test render InputNumber', async () => {
-        render(<InputNumber id="test" options={ { value: 5, 'aria-label': 'Test input', onChange: () => {} } } />);
-        const input = await screen.findByRole('textbox', { name: 'Test input' });
+test('Test render InputNumber', async () => {
+    render(<InputNumber id="test" options={ { value: 5, 'aria-label': 'Test input', onChange: () => {} } } />);
+    const input = await screen.findByRole('spinbutton ', { name: 'Test input' });
 
-        expect(input).toBeInTheDocument();
-        expect(input).toHaveValue(5);
-    });
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue(5);
+});
 
-    test('Test user event InputNumber', async () => {
-        render(<InputNumber id="test" options={ { value: 0, 'aria-label': 'Test input', onChange: () => {} } } />);
-        const input = await screen.findByRole('textbox', { name: 'Test input' });
+test('Test render InputDatetime', async () => {
+    render(<InputDatetime id="test" options={ { value: '2020-05-12T23:50:21.817', 'aria-label': 'Test input', onChange: () => {} } } />);
+    const input = await screen.findByRole('spinbutton ', { name: 'Test input' });
 
-        userEvent.clear(input);
-        userEvent.type(input, '15');
-        expect(input).toHaveValue(15);
-    });
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('2020-05-12T23:50:21.817');
 });
 
 test('Test render InputSubmit', async () => {
